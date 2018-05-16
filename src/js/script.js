244e3fd5c79d9a44a466c283a8fe7c7d9f624329
sashaglobenko <sasha.globenko70@gmail.com>
2018-05-16 16:17:16 +0300

    Moving of ship

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,17 @@
 $(function () {
   var img = new Image();
   img.src = 'img/bg.png';
+  var shipImg = new Image();
+  shipImg.src = 'img/ship.png';
   var example = document.getElementById("background"),
     ctx = example.getContext('2d');
+  var qqz = document.getElementById("ship"),
+    ctx1 = qqz.getContext('2d');
   example.width  = 600;
   example.height = 360;
+  qqz.width = 600;
+  qqz.height = 360;
   function background (x, y, width, height, img) {
-    console.log(img)
     this.x = x;
     this.y = y;
     this.width = width;
@@ -15,26 +20,46 @@ $(function () {
       ctx.drawImage(img, x, y)
     }
   }
-  function ship(x, y) {
+  function ship(x, y, width, height, img) {
+    console.log('>>>>', x, y)
     this.x = x;
     this.y = y;
     this.drawShip = function () {
-      ctx.drawImage()
+      ctx1.drawImage(img, x, y)
     }
   }
+
   var image = new background(0, 0, 600, 360, img);
-  // console.log('image', image);
+  var ship = new ship(270, 320, 40, 26, shipImg)
   image.draw();
+  ship.drawShip();
+
+  var prev = performance.now();
+  var times = 0;
+
   function update () {
     image.y += 1;
+    ctx1.drawImage(shipImg, ship.x, ship.y)
     ctx.drawImage(img, 0, image.y)
     ctx.drawImage(img, 0, image.y - example.height)
     if (image.y >= example.height) {
       image.y = 0;
     }
-    console.log(image.y)
   }
   var timer = setInterval( function () {
     update();
-  }, 1000/24)
+  }, 1000/60)
+
+  $(document).keydown(function (eventObject) {
+    if(eventObject.which === 37) {
+      ctx1.clearRect(ship.x, ship.y, 40, 26)
+      ship.x = ship.x - 5;
+      ctx1.drawImage(shipImg, ship.x, ship.y);
+    }
+    else if(eventObject.which === 39) {
+      ctx1.clearRect(ship.x, ship.y, 40, 26)
+      ship.x = ship.x + 5;
+      ctx1.drawImage(shipImg, ship.x, ship.y);
+    }
+  })
 });
