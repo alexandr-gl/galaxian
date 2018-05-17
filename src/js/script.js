7a23bd92eee1554797aaef69f003c7517972b615
sashaglobenko <sasha.globenko70@gmail.com>
2018-05-17 10:59:21 +0300

    Restructure code

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,29 @@
 $(function () {
-  var img = new Image();
-  img.src = 'img/bg.png';
-  var shipImg = new Image();
-  shipImg.src = 'img/ship.png';
-  var bulletImg = new Image ();
-  bulletImg.src = 'img/bullet.png'
-  var example = document.getElementById("background"),
-    ctx = example.getContext('2d');
-  var shipCanvas = document.getElementById("ship"),
-    ctx1 = shipCanvas.getContext('2d');
-  var bulletCanvas = document.getElementById("bullet"),
-    ctx2 = bulletCanvas.getContext('2d');
-  var bulletsArr = [];
-  example.width  = 600;
-  example.height = 360;
-  shipCanvas.width = 600;
-  shipCanvas.height = 360;
-  bulletCanvas.width = 600;
-  bulletCanvas.height = 360;
+  var game = new Game();
+  var imageStore = new function () {
+    this.img = new Image();
+    this.shipImg = new Image();
+    this.bulletImg = new Image();
 
-  function bullet (x, y, img) {
+    this.img.src = 'img/bg.png';
+    this.bulletImg.src = 'img/bullet.png'
+    this.shipImg.src = 'img/ship.png';
+  }
+
+  function Game() {
+    this.bg = document.getElementById("background");
+    this.shipCanvas = document.getElementById("ship");
+
+    this.bg.width = 600;
+    this.bg.height = 360;
+    this.shipCanvas.width = 600;
+    this.shipCanvas.height = 360;
+
+    this.bgCtx = this.bg.getContext('2d')
+    this.shipCtx = this.shipCanvas.getContext('2d')
+  }
+
+  function background (x, y, img) {
     // this.alive = false;
     // function spawn(x, y,) {
     //   this.x = x;
@@ -29,17 +33,17 @@ $(function () {
     this.x = x;
     this.y = y;
     this.draw = function () {
-      ctx2.drawImage(img, x, y)
+      game.bgCtx.drawImage(imageStore.img, x, y)
     }
   }
-  function background (x, y, width, height, img) {
+  function bullet (x, y, width, height, img) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.draw = function () {
-      ctx.drawImage(img, x, y)
-      ctx.drawImage(img, x+20, y)
+      game.bgCtx.drawImage(imageStore.bulletImg, x, y)
+      game.bgCtx.drawImage(imageStore.bulletImg, x+20, y)
     }
   }
   function ship(x, y, width, height, img) {
@@ -47,12 +51,12 @@ $(function () {
     this.x = x;
     this.y = y;
     this.drawShip = function () {
-      ctx1.drawImage(img, x, y)
+      game.shipCtx.drawImage(imageStore.shipImg, x, y)
     }
   }
 
-  var image = new background(0, 0, 600, 360, img);
-  var ship = new ship(270, 320, 40, 26, shipImg)
+  var image = new background(0, 0, 600, 360, imageStore.img);
+  var ship = new ship(270, 320, 40, 26, imageStore.shipImg)
   var bul1 = null
   var bul2 = null
   image.draw();
@@ -61,21 +65,21 @@ $(function () {
 
   function update () {
     image.y += 1;
-    ctx1.drawImage(shipImg, ship.x, ship.y)
-    ctx.drawImage(img, 0, image.y)
-    ctx.drawImage(img, 0, image.y - example.height)
-    if (image.y >= example.height) {
+    game.shipCtx.drawImage(imageStore.shipImg, ship.x, ship.y)
+    game.bgCtx.drawImage(imageStore.img, 0, image.y)
+    game.bgCtx.drawImage(imageStore.img, 0, image.y - game.bg.height)
+    if (image.y >= game.bg.height) {
       image.y = 0;
     }
   }
 
   function updateBullet () {
-    ctx2.clearRect(bul1.x, bul1.y, 2, 14)
-    ctx2.clearRect(bul2.x, bul2.y, 2, 14)
+    game.shipCtx.clearRect(bul1.x, bul1.y, 2, 14)
+    game.shipCtx.clearRect(bul2.x, bul2.y, 2, 14)
     bul1.y = bul1.y - 2;
     bul2.y = bul2.y - 2;
-    ctx2.drawImage(bulletImg, bul1.x, bul1.y)
-    ctx2.drawImage(bulletImg, bul2.x, bul2.y)
+    game.shipCtx.drawImage(imageStore.bulletImg, bul1.x, bul1.y)
+    game.shipCtx.drawImage(imageStore.bulletImg, bul2.x, bul2.y)
   }
 
   var timer = setInterval( function () {
@@ -88,21 +92,19 @@ $(function () {
   $(document).keydown(function (eventObject) {
     if(eventObject.which === 37 && ship.x > 0) {
       console.log('X', ship.x);
-      ctx1.clearRect(ship.x, ship.y, 40, 26)
+      game.shipCtx.clearRect(ship.x, ship.y, 40, 26)
       ship.x = ship.x - 5;
-      ctx1.drawImage(shipImg, ship.x, ship.y);
+      game.shipCtx.drawImage(imageStore.shipImg, ship.x, ship.y);
     }
     else if(eventObject.which === 39 && ship.x < 560) {
-      ctx1.clearRect(ship.x, ship.y, 40, 26)
+      game.shipCtx.clearRect(ship.x, ship.y, 40, 26)
       ship.x = ship.x + 5;
-      ctx1.drawImage(shipImg, ship.x, ship.y);
+      game.shipCtx.drawImage(imageStore.shipImg, ship.x, ship.y);
     }
     else if(eventObject.which === 32)
     {
-      bul1 = new bullet(ship.x + 5, ship.y - 6, bulletImg)
-      bul2 = new bullet(ship.x + 33, ship.y - 6, bulletImg)
-      bulletsArr.push(bul1, bul2);
-      console.log('bulletsArr', bulletsArr);
+      bul1 = new bullet(ship.x + 5, ship.y - 6, imageStore.bulletImg)
+      bul2 = new bullet(ship.x + 33, ship.y - 6, imageStore.bulletImg)
       bul1.draw();
       bul2.draw();
     }null
