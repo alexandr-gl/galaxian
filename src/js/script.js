d9995cfeae80f9c0367265d1bc2d40673fe804d3
sashaglobenko <sasha.globenko70@gmail.com>
2018-05-21 09:20:23 +0300

    Delete coments. Delete logs. Delete unused code and variables

--- src/js/script.js
+++ src/js/script.js
@@ -41,16 +41,9 @@ $(function () {
           this.pool[i] = bullet;
         }
       }
-      // else if (type === 'enemies') {
-      //   for (let i = 0; i < 18; i++) {
-      //     var enemy = new Enemies();
-      //     this.pool[i] = enemy;
-      //   }
-      // }
     }
     this.get = function (x, y) {
       if(!this.pool[size - 1].alive) {
-        //console.log('CHECK pool', this.pool)
         this.pool[size - 1].spawn(x, y);
         this.pool.unshift(this.pool.pop());
       }
@@ -75,14 +68,14 @@ $(function () {
     }
   }
 
-  function background (x, y, img) {
+  function background (x, y) {
     this.x = x;
     this.y = y;
     this.draw = function () {
       game.bgCtx.drawImage(imageStore.img, x, y)
     }
   }
-  function Bullet (x, y, width, height, img) {
+  function Bullet (x, y, width) {
     this.alive = false;
     this.spawn = function(x, y) {
       this.x = x;
@@ -91,24 +84,15 @@ $(function () {
     };
     this.x = x;
     this.y = y;
-    this.width = width;
-    this.height = 360;
     this.draw = function () {
       game.shipCtx.clearRect(this.x, this.y, 2, 14)
-      console.log('CHECK ALIVE IN DRAW', this.alive)
       this.y -= 2
       if (this.y <= 0) {
         return true;
       }
       else {
-        console.log('DRAW COOEDINATES', this.x, this.y)
         game.shipCtx.drawImage(imageStore.bulletImg, this.x, this.y);
       }
-      // if (this.y === arr[arr.length - 1].y)
-      // {
-      //   arr[arr.length - 1].alive = false;
-      // }
-
     }
     this.clear = function () {
       this.x = 0;
@@ -122,11 +106,6 @@ $(function () {
     this.x = x;
     this.y = y;
     this.alive = true;
-    // this.spawn = function (x, y) {
-    //   this.x = x;
-    //   this.y = y;
-    //   this.alive = true;
-    // }
     this.draw = function(alive) {
       if(alive === true) {
         game.mainCtx.drawImage(imageStore.enemyImg, this.x, this.y)
@@ -138,13 +117,11 @@ $(function () {
   }
 
   function InitEnemies(x, y) {
-    console.log('WE ARE HERE')
     this.poolEn = [];
     this.x = x;
     this.y = y;
     this.init = function () {
       for (let i = 0; i < 18; i++) {
-        console.log('INIT ENEMIES', this.x, this.y)
         var enemy = new Enemies(this.x, this.y, i)
         this.x += 58;
         if ((i + 1) % 6 === 0 && i !== 0) {
@@ -152,24 +129,11 @@ $(function () {
           this.y += 38;
         }
         this.poolEn.push(enemy);
-        // this.poolEn[i].draw();
       }
-      console.log('>>>>', this.poolEn)
     }
-    // this.animate = function() {
-    //   for(let i=0; i < size; i++) {
-    //     if(this.poolEn[i].alive) {
-    //       if(this.poolEn[i].draw()) {
-    //         this.poolEn[i].clear();
-    //         this.poolEn.push((this.pool.splice(i,1))[0])
-    //       }
-    //     }
-    //     else break;
-    //   }
-    // }
   }
 
-  function ship(x, y, width, height, img) {
+  function ship(x, y) {
     this.x = x;
     this.y = y;
     this.drawShip = function () {
@@ -179,9 +143,7 @@ $(function () {
 
   var image = new background(0, 0, 600, 360, imageStore.img);
   var ship = new ship(270, 320, 40, 26, imageStore.shipImg)
-  var bul1 = null;
   pool.init('bullets');
-  //pool.init('enemies');
   enemies.init();
   var arr;
 
@@ -197,7 +159,6 @@ $(function () {
   }
   var timer = setInterval( function () {
     update();
-    top:
     for(let i = 0; i < 30; i++)
     {
       if(pool.pool.length !== 0 && pool.pool[i].alive !== false && pool.pool[i].y > 0) {
@@ -205,7 +166,6 @@ $(function () {
           return pool.pool[i].x >= item.x && pool.pool[i].x <= item.x + 38 && item.y + 28 === pool.pool[i].y && item.alive !== false;
         })
         pool.pool[i].draw();
-        console.log('CHECK ARRAY AFTER FILTER', arr)
         if(arr.length !== 0) {
           arr[arr.length - 1].alive = false;
           game.shipCtx.clearRect(pool.pool[i].x, pool.pool[i].y, 2, 14)
@@ -224,7 +184,6 @@ $(function () {
 
   $(document).keydown(function (eventObject) {
     if(eventObject.which === 37 && ship.x > 0) {
-      console.log('X', ship.x);
       game.shipCtx.clearRect(ship.x, ship.y, 40, 26)
       ship.x = ship.x - 5;
       game.shipCtx.drawImage(imageStore.shipImg, ship.x, ship.y);
